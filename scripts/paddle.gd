579e091e8f32125b7ec02b908ac1d11051eeecef
narutocyx <narutocyxlive@163.com>
2025-02-24 20:08:43 +0800

    hello world

---
+++ scripts/paddle.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var speed:float = 400.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_left"):
+		position.x -= speed * delta
+	if Input.is_action_pressed("move_right"):
+		position.x +=  speed * delta
