7d8ea86c53fd4773922abc9f17e5018da254f45b
narutocyx <narutocyxlive@163.com>
2025-03-07 16:08:24 +0800

    111

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -7,4 +7,4 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_left"):
 		position.x -= speed * delta
 	if Input.is_action_pressed("move_right"):
-		position.x +=  speed * delta
+		position.x += speed * delta
